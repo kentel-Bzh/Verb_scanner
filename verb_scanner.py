3c94a77efd95ce0119681e8145d2da15fdcf03ee
Krzysiek Szymanski <krzysiek.szymanski.kw@gmail.com>
2023-06-08 00:20:23 +0200

    Fix verb search.

--- verb_scanner.py
+++ verb_scanner.py
@@ -3,6 +3,7 @@ import json
 text = str(input("enter text: "))
 
 #convert json verb list to dict.
+conjugaisons = []
 with open("verbes.json") as verbs:
     conjugaisons = json.load(verbs)
     
@@ -10,9 +11,17 @@ with open("verbes.json") as verbs:
 text_list = text.split(" ")
 print(text_list)
 
-for word in text_list:
-    for key, value in conjugaisons.items():
-        if word == conjugaisons[value]:
-            print(word)
-    else:
-        print("pas de verbe listé")
+found = False
+for word in text_list:    
+    for verb in conjugaisons:
+        # Need to add search for other keys, not just indicatif present
+        if 'indicatif' in verb:
+            indic = verb['indicatif']['prĂ©sent']
+            if word in indic:
+                print("Found verb:")
+                print(verb)
+                found |= True
+                break
+            
+if found == False:
+    print("pas de verbe listé")
