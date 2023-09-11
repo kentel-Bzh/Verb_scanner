904809c479d24501fafd2de09f3915d40e78c8fd
kentel-Bzh <gillesquentel@hotmail.com>
2023-09-11 22:09:27 +0200

    minor correction

--- verb_scanner.py
+++ verb_scanner.py
@@ -16,7 +16,7 @@ for word in text_list:
     for verb in conjugaisons:
         # Need to add search for other keys, not just indicatif present
         if 'indicatif' in verb:
-            indic = verb['indicatif']['prĂ©sent']
+            indic = verb['indicatif']['présent']
             if word in indic:
                 print("Found verb:")
                 print(verb)
